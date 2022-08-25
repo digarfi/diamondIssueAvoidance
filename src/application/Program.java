94b6048a0d46619810e5d1b772b7218d8c80f6f4
digarfi <digarfi@gmail.com>
2022-08-25 19:14:05 -0300

    Solution avoiding multiple inheritance

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,21 +2,28 @@ package application;
 
 
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		 Printer p =new Printer ("10588");
+		 ConcretePrinter p =new ConcretePrinter ("1058");
 		 p.processDoc("My Letter");
 		 p.print("My Letter");
 		 
 		 System.out.println();
-		 Scanner s = new Scanner ("20300" );
+		 ConcreteScanner s = new ConcreteScanner ("2030" );
 		 s.processDoc("email");
 		 System.out.println("Scan result: " + s.scan());
+		 
+		 System.out.println();
+		 ComboDevice c = new ComboDevice("2081");
+		 c.processDoc("Dissertation");
+		 c.print("Dissertation");
+		 System.out.println("Scan Result: " + c.scan());
+			 
 	}
-
 }
